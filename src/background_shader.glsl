d0f5c61c922ff1a03e077795a075cdc2ee941d64
Kamal Ahmad <kamalahmad22@pm.me>
2023-10-10 10:59:31 +0500

    Local memory shading; commit 1

--- src/background_shader.glsl
+++ src/background_shader.glsl
@@ -11,10 +11,14 @@ uniform uint uWidth;
 uniform uint uHeight;
 layout(binding=0) uniform atomic_uint converged;
 
+
+// For an 8x8 workgroup, we need 9x9 pixels of the region
+shared float depths[81];
 // Clamped load, make sure nothing goes out of bounds
 vec4 c_load(readonly image2D image, ivec2 coords) {
-    ivec2 clamped = ivec2(clamp(coords.x, 0, uWidth), clamp(coords.y, 0, uHeight));
-    return imageLoad(image, clamped);
+    ivec2 clamped = ivec2(clamp(coords.x, 0, 8), clamp(coords.y, 0, 8));
+    return vec4(vec3(depths[clamped.y * 9 + clamped.x]), 1.0);
+    //return imageLoad(image, clamped);
 }
 
 float apply_kernel(float kernel[9], float neighbors[9]) {
@@ -30,16 +34,32 @@ void main() {
     ivec2 i = ivec2(gl_GlobalInvocationID.x, gl_GlobalInvocationID.y);
     vec4 load = imageLoad(input_image, i);// / 255.0;
     vec4 depth = imageLoad(input_depth, i);
+    ivec2 il = ivec2(gl_LocalInvocationID.x + 1, gl_LocalInvocationID.y + 1);
+    if (il.x == 0 && il.y == 0) {
+
+        for (int xx = -1; xx <= 1; xx++) {
+            for (int yy = -1; yy <= 1; yy++) {
+            }
+        }
+        depths[0] = imageLoad(input_depth, i + ivec2(-1, -1)).r;
+        depths[1] = imageLoad(input_depth, i + ivec2(0, -1)).r;
+        depths[2] = imageLoad(input_depth, i + ivec2(0, -1)).r;
+
+    }
+    depths[(gl_LocalInvocationID.y + 1) * 9 + gl_LocalInvocationID.x + 1] = depth.r;
+
 
     ivec2 offsets[9] = {
         ivec2(-1, -1), ivec2(0, -1), ivec2(1, -1),
         ivec2(-1, 0), ivec2(0, 0), ivec2(1, 0),
         ivec2(-1, 1), ivec2(0, 1), ivec2(1, 1)
     };
+
+    barrier();
     float neighbors[9] = {
-        c_load(input_depth, i + offsets[0]).r, c_load(input_depth, i + offsets[1]).r, c_load(input_depth, i + offsets[2]).r,
-        c_load(input_depth, i + offsets[3]).r, c_load(input_depth, i + offsets[4]).r, c_load(input_depth, i + offsets[5]).r,
-        c_load(input_depth, i + offsets[6]).r, c_load(input_depth, i + offsets[7]).r, c_load(input_depth, i + offsets[8]).r
+        c_load(input_depth, il + offsets[0]).r, c_load(input_depth, il + offsets[1]).r, c_load(input_depth, il + offsets[2]).r,
+        c_load(input_depth, il + offsets[3]).r, c_load(input_depth, il + offsets[4]).r, c_load(input_depth, il + offsets[5]).r,
+        c_load(input_depth, il + offsets[6]).r, c_load(input_depth, il + offsets[7]).r, c_load(input_depth, il + offsets[8]).r
     };
 
     if (abs(depth.r) > 1e-5) {
@@ -99,7 +119,7 @@ void main() {
 
         float prod = sum1 * sum2 * sum3 * sum4 * sum5 * sum6 * sum7 * sum8;
 
-        if (abs(prod) > 1e-6) {
+        if (abs(prod) > 1e-5) {
             int min_idx = 0;
             float min_depth = 9999.0;
             for (int i = 0; i < 9; i++) {
